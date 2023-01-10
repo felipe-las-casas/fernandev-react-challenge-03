9900a14c05f90d50904798a69a050f0bbd86f253
felipe-las-casas <felipelascasas7@gmail.com>
2023-01-10 12:48:52 -0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -35,25 +35,88 @@ Ao enviar, deve-se apresentar um alert javascript com sucesso, limpar todos os c
 do formulário e zerar a barra de progresso novamente.
 */
 
-function App() {
-  return (
+import { useState } from "react";
+function App () {
+
+
+    const [data, setData] = useState({
+      fullName: '',
+      email:'',
+      maritalStatus:'',
+      genre: '',
+    })
+
+    const handleChange = (event) => {
+      console.log(event)
+      const {name, value} = event.target
+      setData((prev) => {
+        const newData = {...prev, [name]: value}
+        console.log(newData)
+
+        return newData
+      })
+    }
+
+    const calculateProgress = () => {
+      let value = 0;
+      let amountToAdd = 25;
+
+      if(data.fullName) {
+        const explodeString = data.fullName.split(' ')
+        if(explodeString[1]) {
+          value += amountToAdd;
+        }
+      }
+      if(data.email) {
+        let pattern =  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(pattern.test(data.email)) {
+          value += amountToAdd;
+        }
+      }
+      if(data.maritalStatus) {
+        value += amountToAdd;
+      }
+      if(data.genre) {
+        value += amountToAdd;
+      }
+
+      console.log({ value })
+
+      return value;
+    }
+
+    const handleClick = () => {
+      alert('Enviado com sucesso!')
+      setData({
+        fullName: '',
+        email:'',
+        maritalStatus:'',
+        genre: '',
+      })
+    }
+
+
+    return (
+
     <div className='App'>
       <h3>desafio fernandev</h3>
       <h1>progresso do formulário</h1>
 
       <main>
-        {/* crie a barra de progresso aqui */}
+        <div className="bar-container">
+          <div className="bar" style={{width: `${calculateProgress()}%`}}></div>
+        </div>
         <div className='form-group'>
           <label htmlFor=''>Nome Completo</label>
-          <input />
+          <input name='fullName' value={data.fullName} onChange={handleChange}/>
         </div>
         <div className='form-group'>
           <label htmlFor=''>E-mail</label>
-          <input />
+          <input name='email' value={data.email} onChange={handleChange}/>
         </div>
         <div className='form-group'>
           <label htmlFor=''>Estado Civil</label>
-          <select>
+          <select name='maritalStatus' onChange={handleChange}>
             <option value=''>- selecione...</option>
             <option value='solteiro'>Solteiro</option>
             <option value='casado'>Casado</option>
@@ -64,14 +127,24 @@ function App() {
           <label htmlFor=''>Gênero</label>
           <div className='radios-container'>
             <span>
-              <input type='radio' /> Masculino
+              <input type='radio'
+              name='genre'
+              value='masculino'
+              onChange={handleChange}
+              checked={data.genre === 'masculino'} /> Masculino
             </span>
             <span>
-              <input type='radio' /> Feminino
+              <input type='radio'
+               name='genre'
+               value='feminino'
+               onChange={handleChange}
+               checked={data.genre === 'feminino'}  /> Feminino
             </span>
           </div>
         </div>
-        <button>Enviar Formulário</button>
+        <button
+        onClick={handleClick} 
+        disabled={calculateProgress() !== 100}>Enviar Formulário</button>
       </main>
     </div>
   );
